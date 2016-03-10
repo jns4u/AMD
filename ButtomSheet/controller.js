1b93e515e7fa40bce2d9cfd1909818f029a13582
Chanakya <chanakya@localhost.localdomain>
2016-03-10 12:47:54 +0530

    3rd commit

--- ButtomSheet/controller.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('app', ['ngMaterial']);
-app.controller('MyController', function($scope, $mdBottomSheet) {
-  $scope.openBottomSheet = function() {
-    $mdBottomSheet.show({
-      template: '<md-bottom-sheet>Hello!</md-bottom-sheet>'
-    });
-  };
-});
